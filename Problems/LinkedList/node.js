ab744245ea878a9de7ba6ae66fa7ee2cad335dc2
Ashish <ashishagnihotri2k@gmail.com>
2023-12-08 10:09:56 +0530

    added singly linked list push method.

--- Problems/LinkedList/node.js
+++ Problems/LinkedList/node.js
@@ -5,8 +5,41 @@ class Node{
     }
 }
 
+class SinglyLinkedList{
+    constructor(){
+        this.head=null;
+        this.tail=null;
+        this.length=0;
+    }
+    push(val){
+       var node =new Node(val);
+       if(this.length==0){
+        this.head=node;
+        this.tail=node;
+        this.length++;
+       }
+       else {
+        let current=this.head;
+        while(current.next){
+            current=current.next;
+        }
+        this.tail=node
+        current.next=node;
+        this.length++;
+       }
+    }
+}
+
 
 var first=new Node("hey");
 first.next=new Node("there");
+
+var list=new SinglyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+
+console.log(list.head.next, list.length, list.tail)
 
 console.log(first.next)
